a12cd110a975fefa8373f2107ffc8aeac91d0718
RGB-2025 <205908174+RGB-2025@users.noreply.github.com>
2025-04-21 20:12:57 +0800

    Create gameboard module

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 // Note: X is 1, O is 0, blank is null
 
 const Player = (function () {
-    const createPlayer = (name, type) => {name, type}
+    const createPlayer = (name, type) => {name, type};
 
     return {createPlayer};
+})();
+
+const GameBoard = (function () {
+    let grid = [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null]
+    ];
+
+    const setCell = (row, column, player) =>
+        grid[row][column] = player.type;
+
+    const getGrid = () => grid;
+
+    return {setCell, getGrid};
 })();
